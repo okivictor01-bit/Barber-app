e1bea957ce01264e024409496bda905de129749c
okivictor01-bit <okivictor01@gmail.com>
2026-08-11 05:16:24 +0100

    Add files via upload

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,14 @@
-ghjj
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/dashboard/', '/api/'],
+      },
+    ],
+    sitemap: 'https://barbflow.shop/sitemap.xml',
+  };
+}
