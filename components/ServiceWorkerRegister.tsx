525cda0b230bc396b2618e108bad648a96338c4d
okivictor01-bit <okivictor01@gmail.com>
2026-08-16 21:05:48 +0100

    Add files via upload

--- components/ServiceWorkerRegister.tsx
+++ components/ServiceWorkerRegister.tsx
@@ -1,1 +1,15 @@
-jdjjd
+'use client';
+
+import { useEffect } from 'react';
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {
+        // Registration failures (unsupported browser, etc.) shouldn't break the app.
+      });
+    }
+  }, []);
+
+  return null;
+}
